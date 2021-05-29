f7f86b81f80e25cb57d88c3fa6c8af611fdf0188
imbf <qowhdwlsdk@gmail.com>
2021-05-29 17:45:05 +0900

    Polish APM API

--- server/apm-api/src/main/java/com/capstone/apm/api/repository/NodeRepository.java
+++ server/apm-api/src/main/java/com/capstone/apm/api/repository/NodeRepository.java
@@ -3,5 +3,9 @@ package com.capstone.apm.api.repository;
 import com.capstone.apm.api.Node;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface NodeRepository extends MongoRepository<Node, String> {
+
+    Optional<Node> findByServiceName(String serviceName);
 }
